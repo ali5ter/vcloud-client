e2cada369b5026cb9c411f4b2953b3e2efb5489f
Alister Lewis-Bowen <alister@different.com>
2012-12-07 12:41:14 -0500

    successfully retrieved data model

--- www/js/vcloud.js
+++ www/js/vcloud.js
@@ -29,7 +29,7 @@
         // TODO: /versions API call in SDK bootstrap can take over a minute to
         // complete!! Can this be bypassed?
 
-        // Callback on successful bootstrap
+        // Handle successful bootstrap - just once :)
         vcd.once(vmware.events.cloud.INITIALIZATION_COMPLETE, function() {
             console.log('SDK init complete');
             $('.form-login').show();
@@ -38,15 +38,24 @@
             else logout();
         });
 
-        // Register callback for SDK login method
+        // Handler for SDK login method
         vcd.register(vmware.events.cloud.LOGIN, onLogin);
+
+        // Handler for SDK refresh of data model
+        vcd.register(vmware.events.cloud.REFRESH_COMPLETE, onRefresh);
+
+        // Handler for SDK task start and completion
+        vcd.register(vmware.events.cloud.TASK_START, function() { console.log('SDK task started'); });
+        vcd.register(vmware.events.cloud.TASK_COMPLETE, function() { console.log('SDK task complete'); });
+
+        // Handler for SDK errors
+        vcd.register(vmware.events.cloud.ERROR, function(e) { console.log('SDK error: '+ e.eventData); });
 
         // Register callback to initiate login
         $('.form-login').submit(login);
 
         // Register callback on logout link
         $('.logout').click(logout)
-        //$('.logout').click(function () { logout(); })
     }
 
     /**
@@ -106,11 +115,51 @@
 
     /**
      * @method: initWorkspace
-     * Render the authrnticated workspace
+     * Initialise the authrnticated UI
      */
     function initWorkspace () {
         $('.navbar').show();
         $('.machines').show();
+
+        // Restore vcd data model so we have some data to work with
+        // while SDK is refreshing it
+        if (localStorage.vcdData) {
+            vcd.loadCache(localStorage.vcdData);
+            updateWorkspace();
+        }
+    }
+
+    /**
+     * @method: onRefresh
+     * Store the data model and refresh data in the UI
+     */
+    function onRefresh () {
+        console.log('SDK refreshed data model');
+
+        // Save this updated data model so we can restore it and not block
+        // the UI logic from rendering the workspace
+        localStorage.vcdData = vcd.saveCache();
+
+        updateWorkspace();
+    }
+
+    /**
+     * @method: updateWorkspace
+     * Update the data in the UI
+     * TODO: Might be nice to use a MVC/MVVM pattern like that provided by
+     *       knockout.js
+     */
+    function updateWorkspace () {
+        var vapps = vcd.getVApps(vcd.SORTBY.DATE),
+            tasks = vcd.taskHistory().slice(0, 10),
+            metrics = vcd.metrics(),
+            vapp = {},
+            vm = {};
+
+        for (var i=0; i<vapps.length; i++) {
+            vapp = vapps[i];
+            console.log('vApp name: '+ vapp.getName() +' VMs: '+ vapp.getNumberOfVMs());
+        }
     }
 
     init();
